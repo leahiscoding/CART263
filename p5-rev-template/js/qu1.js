15d1def64711b3caad62fbf49963845502efc8fb
Leah Song <leahhjsong@gmail.com>
2026-01-14 20:28:52 -0500

    edited question 1 and question 2

--- p5-rev-template/js/qu1.js
+++ p5-rev-template/js/qu1.js
@@ -1,5 +1,6 @@
 "use strict";
 
+let x = 30;
 
 function setup() {
     createCanvas(400, 400);
@@ -9,11 +10,11 @@ function setup() {
 function draw (){
     push();
     fill(255, 0, 0);
-    ellipse(30, 30, 50, 50);
+    ellipse(x, x, 50, 50);
     fill (0, 255, 0);
-    ellipse (80, 80, 70, 70);
+    ellipse (x+50, x+50, 70, 70);
     fill (0, 0, 255);
-    ellipse (145, 145, 90, 90);
+    ellipse (x+115, x+115, 90, 90);
     pop ();
 }
 
--- p5-rev-template/js/qu2.js
+++ p5-rev-template/js/qu2.js
@@ -4,6 +4,9 @@
 function setup() {
     createCanvas(400, 400);
     background(0);
+}
+
+function draw (){
     drawEllipse(40, 40, 60, 60, 200, 0, 0);
     drawEllipse(90, 90, 60, 60,0, 200, 0);
     drawEllipse(145, 145, 60, 60, 0, 0, 200);
@@ -12,6 +15,7 @@ function setup() {
 function drawEllipse(x,y,w,h,r,g,b){
     // reference to the previous code in qu1.js
     push();
+    noStroke ();
     fill(r, g, b);
     ellipse(x, y, w, h);
     pop ();
