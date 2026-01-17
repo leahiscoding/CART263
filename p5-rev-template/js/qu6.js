f303db5b873bb7436b0e1ec80d80707401cc663c
Leah Song <leahhjsong@gmail.com>
2026-01-17 16:21:27 -0500

    completed question 6

---
+++ p5-rev-template/js/qu6.js
@@ -0,0 +1,30 @@
+
+
+function setup () {
+    createCanvas (600,600);
+    background (0);
+}
+
+function draw (){
+    
+    fill (255);
+    textSize (32);
+    text ("test", width / 2, height / 2);
+    
+    translate(25, 25)
+    textSize (16);
+    textAlign (LEFT, TOP);
+    for (let i = 0 ; i < 10; i ++)
+        {
+            let x = 20 * i;
+            // because we're incrementing 20 by every time
+            text (i, x, 0);
+            // the text in here is just whatever the value of i is
+        }
+    for (let i = 0; i < 15 ; i ++)
+    {
+        let y = 30 * i;
+        text (i, 0, y);
+    
+    }
+}
